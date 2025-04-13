fd6b00024cf96d2e5805b0d5a0c5bbc232db40fd
David Irvin <davidirvin47@gmail.com>
2025-04-12 18:16:05 -0600

    Add a few testcases to the hook

--- src/hooks/useWebSocket.test.ts
+++ src/hooks/useWebSocket.test.ts
@@ -1,21 +1,37 @@
-import { vi } from "vitest"
+import { MockInstance, vi } from "vitest"
 import { renderHook, waitFor } from "@testing-library/react"
 import { useWebSocket } from "./useWebSocket"
 
 describe("useWebSocket", () => {
-  beforeAll(() => {
-    vi.spyOn(global, "WebSocket").mockImplementation(vi.fn())
-  })
+  const webSockerURL = "ws://localhost:8080"
+  const MockWebSocket = vi.fn()
+  let hook: ReturnType<typeof renderHook>
 
-  const { result } = renderHook(() => {
-    useWebSocket("ws://localhost:8080")
+  beforeAll(() => {
+    MockWebSocket.prototype.close = vi.fn()
+    Object.defineProperty(global, "WebSocket", {
+      value: MockWebSocket,
+      writable: true,
+    })
+    hook = renderHook(() => {
+      useWebSocket(webSockerURL)
+    })
   })
 
   it("should return an object with a isConnected value", () => {
-    waitFor(() => expect(result.current).toHaveProperty("isConnected"))
+    waitFor(() => expect(hook.result.current).toHaveProperty("isConnected"))
   })
 
   it("should return an object with a messages value", () => {
-    waitFor(() => expect(result.current).toHaveProperty("messages"))
+    waitFor(() => expect(hook.result.current).toHaveProperty("messages"))
+  })
+
+  it("should instantiate a new WebSocket", () => {
+    expect(MockWebSocket).toHaveBeenCalledWith(webSockerURL)
+  })
+
+  it("should close the WebSocket connection when the component unmounts", () => {
+    hook.unmount()
+    expect(MockWebSocket.prototype.close).toHaveBeenCalled()
   })
 })
